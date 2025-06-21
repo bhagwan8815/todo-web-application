c78b7fb6316ea8f4d2bf631582abddec8d138af0
bhagwan8815 <ranabhagwan8815@gmail.com>
2025-06-21 16:20:08 +0530

    setup of server is updated

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+
+//create instance or object of express
 const app = express();
+
+
+//import dotenv file and use the congiguration 
 require('dotenv').config();
+const PORT = process.env.PORT || 5000
 
-const PORT = process.env.PORT || 4000
+//middlewares 
+app.use(express.json());
+app.use(cors());
 
+//routes
+
+
+//listen or start the applicaiton here
 app.listen(PORT,(req, res)=>{
-    console.log(`app is running on port ${PORT}`)
+    console.log(`app is running on port ${PORT}`.bgMagenta)
 })
-
-app.use('/',(req, res)=>{
-   console.log(" <h1>this is dummy route for checking .</h1>")
+//create a dummy route 
+app.get('/dummy',(req, res)=>{
+   res.send("this is the dummy routes for checking .")
 })
 

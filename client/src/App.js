e1f6bda92c2e06b6d20b33fb013744cb935fb703
bhagwan8815 <ranabhagwan8815@gmail.com>
2025-06-23 22:32:50 +0530

    add routing in react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import { useState } from "react";
 import "./App.css";
+import {Routes, Route} from 'react-router-dom'
+import Landing from "./pages/Landing/Landing";
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Todos from "./pages/Todos/Todos";
+import About from './pages/About/About'
+
 
 function App() {
  
   return(
     <div>
-      <h1 className="text-3xl text-red-400">welcome to the react app with tailwind styles</h1>
+      <Routes>
+     
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/todolist' element={<Todos/>} />
+
+
+      </Routes>
     </div>
   )
 }

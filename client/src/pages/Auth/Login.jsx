748619645704b1545cd6a5c93c0db246d7b069e2
bhagwan8815 <ranabhagwan8815@gmail.com>
2025-06-30 21:07:34 +0530

    registration form

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -19,8 +19,6 @@ export default function Login() {
           ...prevData,
           [name]:value
         }))
-
-
     }
 
 
--- client/src/pages/Auth/SignUp.jsx
+++ client/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const changeHandler=(e)=>{
+    const {name, value}= e.target;
+
+    setFormData((prevData)=>({
+      ...prevData,
+      [name]:value
+    }))
+
+  }
+
+  const signupHandler = (e) => {
+      e.preventDefault();
+    alert(`login data is  ${formData.email}   ${formData.username}`)
+    console.log("form data", formData)
+  };
   return (
     <div>
-      this is signup page
+      <form action="">
+        <div>
+          <label htmlFor="username">UserName:</label>
+          <input
+            type="text"
+            placeholder="Enter your UserName....."
+            name="username"
+            onChange={changeHandler}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            placeholder="Enter your Email...."
+            name="email"
+            onChange={changeHandler}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Enter password"
+            name="password"
+            onChange={changeHandler}
+          />
+        </div>
+
+        <button type="submit" onClick={signupHandler}>
+          SignUp
+        </button>
+      </form>
     </div>
-  )
+  );
 }
